2b0abb8fd691e7acfd890ac5eac2e41e5008e916
Hien Vien <hienmvien@gmail.com>
2016-02-10 15:07:19 -0800

    Output the postprocessing sql commands into the outputted sql file

--- osm2po.sh
+++ osm2po.sh
@@ -6,18 +6,20 @@
 args=("$@")
 country_code=${args[0]}
 osm_file=${args[1]}
+output_sql_file=${country_code}_osm.sql
 
 mvn clean package
-java -cp ./lib/osm2po-core-5.1.0-signed.jar:./target/foxtrot-osm2po-plugins-1.0-jar-with-dependencies.jar de.cm.osm2po.Main cmd=tjsp prefix=${country_code} postp.0.class=io.foxtrot.osm2po.postp.PgRoutingWriter postp.1.class=io.foxtrot.osm2po.postp.PgVertexWriter ${osm_file} | tee ${country_code}_osm.sql
+
+printf "CREATE EXTENSION IF NOT EXISTS POSTGIS;
+CREATE EXTENSION IF NOT EXISTS PGROUTING;\n\n
+" > ${output_sql_file}
+java -cp ./lib/osm2po-core-5.1.0-signed.jar:./target/foxtrot-osm2po-plugins-1.0-jar-with-dependencies.jar de.cm.osm2po.Main cmd=tjsp prefix=${country_code} postp.0.class=io.foxtrot.osm2po.postp.PgRoutingWriter postp.1.class=io.foxtrot.osm2po.postp.PgVertexWriter ${osm_file} >> ${output_sql_file}
 rm -rf ${country_code}
-printf "Run these commands
-1. psql DB_NAME -c 'create extension postgis'
-2. psql DB_NAME -c 'create extension pgrouting'
-3. psql DB_NAME -f ${country_code}_osm.sql
-4. psql DB_NAME
-5. select pgr_createTopology('${country_code}_osm_edges', '0.000001', 'geom_way');
-6. alter table ${country_code}_osm_edges_vertices_pgr add column the_geog GEOGRAPHY(POINT, 4326);
-7. update ${country_code}_osm_edges_vertices_pgr set the_geog = the_geom::geography;
-8. create index idx_${country_code}_osm_edges_vertices_pgr_the_geog ON ${country_code}_osm_edges_vertices_pgr USING GIST(the_geog);
-9. select pgr_analyzeGraph('${country_code}_osm_edges', '0.000001', 'geom_way');
-"
+
+printf "\n\nSELECT pgr_createTopology('${country_code}_osm_edges', '0.000001', 'geom_way');
+ALTER TABLE ${country_code}_osm_edges_vertices_pgr add column the_geog GEOGRAPHY(POINT, 4326);
+UPDATE ${country_code}_osm_edges_vertices_pgr set the_geog = the_geom::geography;
+CREATE INDEX idx_${country_code}_osm_edges_vertices_pgr_the_geog ON ${country_code}_osm_edges_vertices_pgr USING GIST(the_geog);
+" >> ${output_sql_file}
+
+printf "You can now run: psql DB_NAME -f ${country_code}_osm.sql"
